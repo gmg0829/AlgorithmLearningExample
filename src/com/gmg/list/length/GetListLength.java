8265dfa83e740226c536e4af878d87dd9099e0b3
gmg <minggang0829@gmail.com>
2019-02-21 14:35:35 +0800

    commit

--- src/com/gmg/list/length/GetListLength.java
+++ src/com/gmg/list/length/GetListLength.java
@@ -12,13 +12,13 @@ package com.gmg.list.length;
  */
 public class GetListLength {
     public static void main(String[] args) {
-        com.gmg.list.reverse.Node head = new com.gmg.list.reverse.Node(3);
-        com.gmg.list.reverse.Node node1 = new com.gmg.list.reverse.Node(5);
-        com.gmg.list.reverse.Node node2 = new com.gmg.list.reverse.Node(6);
-        com.gmg.list.reverse.Node node3 = new com.gmg.list.reverse.Node(9);
-        com.gmg.list.reverse.Node node4 = new com.gmg.list.reverse.Node(7);
-        com.gmg.list.reverse.Node node5 = new com.gmg.list.reverse.Node(2);
-        com.gmg.list.reverse.Node node6 = new com.gmg.list.reverse.Node(1);
+       Node head = new Node(3);
+       Node node1 = new Node(5);
+       Node node2 = new Node(6);
+       Node node3 = new Node(9);
+       Node node4 = new Node(7);
+       Node node5 = new Node(2);
+       Node node6 = new Node(1);
         head.next = node1;
         node1.next=node2;
         node2.next=node3;
@@ -30,7 +30,7 @@ public class GetListLength {
         System.out.println(listLength);
     }
     // 打印链表的方法，方便test函数
-    public static void printList(com.gmg.list.reverse.Node head) {
+    public static void printList(Node head) {
         while (head != null) {
             System.out.print(head.val + " ");
             head = head.next;
@@ -42,14 +42,14 @@ public class GetListLength {
      * 求单链表中结点的个数
      * 注意检查链表是否为空。时间复杂度为O（n）
      */
-    public static int getListLength(com.gmg.list.reverse.Node head) {
+    public static int getListLength(Node head) {
         // 注意头结点为空情况
         if (head == null) {
             return 0;
         }
 
         int len = 0;
-        com.gmg.list.reverse.Node cur = head;
+      Node cur = head;
         while (cur != null) {
             len++;
             cur = cur.next;
@@ -61,7 +61,7 @@ public class GetListLength {
 // 建立链表结构类 val和next
 class Node{
     int val;
-    com.gmg.list.reverse.Node next;
+    Node next;
     public Node(int val){
         this.val=val;
         this.next=null;
